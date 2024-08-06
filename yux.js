a0df4afdb40f689c5ed69ecc6d5713240cc22c59
kouyacha <134472890+ponamaru@users.noreply.github.com>
2024-08-06 09:10:25 +0900

    Update yux.js

--- yux.js
+++ yux.js
@@ -4,7 +4,7 @@ document.write( '<img src="black2.png" id="black" class="op0" width="120%" heigh
 document.write( '<img src="stick.png" id="stickbody" width="600">' );
 document.write( '<img src="stick.png" id="leftleg" class="leftleg" width="600">' );
 document.write( '<img src="stick.png" id="rightleg" class="rightleg" width="600">' );
-document.write( '<div id="stickface" class="item2"><div class="item rotateX" id="face2" width="600"><img src="stickhead.png" id="stickhead" type="file1" class="red" width="600"><div class="re-wrap" id="stickeye3"><img src="stickeye3.png" id="stickeye" class="re" width="600"></div></div></div>' );
+document.write( '<div id="stickface" class="item2"><div class="item rotateX" id="face2" width="600"><img src="stickhead.png" id="stickhead" type="file1" class="red" width="600"><div class="re-wrap" id="stickeye3"><img src="stickeye3.png" id="stickeye" class="re" width="600"></div><img src="mouth.png" id="stickmouth" class="op0" width="600"></div></div>' );
 
 var y = 300;
 var x = 500;
@@ -36,6 +36,27 @@ document.getElementById( 'rightleg' ).style.left = x -15 + "px";
 }
 
 let intervalID = setInterval(bodylink);
+
+function mouthClass() {
+const facetext = array[nowtext];
+const serifName = facetext.includes('Stick"');
+if(serifName === true) {
+  const mouth = document.getElementById("stickmouth");
+    mouth.classList.remove("op0");
+    mouth.classList.add("mouthM");
+setTimeout(function() {
+    const mouth = document.getElementById("stickmouth");
+    mouth.classList.remove("mouthM");
+    mouth.classList.add("op0");
+}, 2000);
+}else{
+  const mouth = document.getElementById("stickmouth");
+    mouth.classList.remove("mouthM");
+    mouth.classList.add("op0");
+}
+}
+
+addEventListener( "keydown", mouthClass );
 
 function fadeout() {
   const element = document.getElementById("fadein");
